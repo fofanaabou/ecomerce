8b6d81347ff58ea6cc5c51556b6d0f577e97a1e4
FOFANA <aboufofana76@gmail.com>
2021-06-30 10:11:53 +0200

    search

--- frontend/angular-ecommerce/src/app/constants/api-url.ts
+++ frontend/angular-ecommerce/src/app/constants/api-url.ts
@@ -3,5 +3,5 @@ import { environment } from '../../environments/environment';
 export class ApiUrl {
   static PRODUCTS = `${environment.baseUrl}/products` ;
   static PRODUCT_CATEGORY = `${environment.baseUrl}/product_category`;
-  static SEARCH_PRODUCTS = ApiUrl.PRODUCTS + '/search/findByNameContaining?name=';
+  static SEARCH_PRODUCTS = ApiUrl.PRODUCTS + '/search/findByNameContainingIgnoreCase?name=';
 }
